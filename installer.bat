eeda531bb29584ac56c6852b71dc87e124a71a23
jefino9488 <jefinojacob9488@gmail.com>
2024-10-28 21:31:28 +0530

    typo fix

--- installer.bat
+++ installer.bat
@@ -23,7 +23,7 @@ if "%device%" equ "unknown" (
 )
 
 set "compatibleDevice=false"
-for /f "delims=" %%i in (%SCRIPT_PATH%\compactable_list.txt) do (
+for /f "delims=" %%i in (%SCRIPT_PATH%\compatible_list.txt) do (
     if "%device%" equ "%%i" set "compatibleDevice=true"
 )
 
